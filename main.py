97d1c6c4f79f4cbf6d2a9e454568c2a5c143f39c
vikram2024 <140162113+vikram2024@users.noreply.github.com>
2023-12-05 14:18:28 +0530

    main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ parser = argparse.ArgumentParser()
 
 # Adding optional argument
 parser.add_argument("-t", "--text", help="string input")
-parser.add_argument("-l", "--language", help="english or hindi")
 parser.add_argument("-a", "--accent", help="indian, australian, us & uk")
 parser.add_argument("-g", "--gender", help="male or female")
 parser.add_argument("-i", "--index", help="reader index, start from zero.")
+parser.add_argument("-o", "--output", help="output audio file name")
 
 # Read arguments from command line
 args = parser.parse_args()
@@ -19,18 +19,12 @@ args = parser.parse_args()
 # Default values
 text_to_read = "Hi there! I'll read text for you."
 gender = "male"
-language = "english"
 accent = "us"
+output_file = "output.mp3"
 
 # if text args passed in cli
 if args.text:
     text_to_read = args.text
-
-# if language args passed in cli
-if args.language:
-    language = args.language
-    if language == "hindi":
-        accent = "indian"
 
 # if accent args passed in cli
 if args.accent:
@@ -40,72 +34,27 @@ if args.accent:
 if args.gender:
     gender = args.gender
 
-# filters the languages and accents
-def filter_rule(voice, gender, language, accent, default):
-    if default:
-        return voice.gender in gender and voice.languages[0] == (language + '_' + accent)
-    return voice.languages[0] == (language + '_' + accent) or voice.name in ["default", "Alex"]
+# if output args passed in cli
+if args.output:
+    output_file = args.output
 
-
-# Filtering voices based on given critaria
-def filter_voice(voices, gender, language, accent, default=True):
-    filter_list = [voice for voice in voices if filter_rule(
-        voice, gender, language, accent, default)]
-    if len(filter_list):
-        return filter_list
-    return filter_voice(voices, gender, language, accent, False)
-
-
-# update reader's language, accent and gender
-def update_language(reader, language, accent, gender):
-    # Audio Type Selection Criteria
-    languages = {"english": "en", "hindi": "hi"}
-    genders = {"male": ["VoiceGenderMale", "male"], "female": ["VoiceGenderFemale", "female"], "none": ["None"]}
-    accents = {"indian": "IN", "us": "US", "australian": "AU", "uk": "GB"}
-
-    # getting list of filtered voices based on selection criteria
-    filtered_voice_list = filter_voice(reader.getProperty(
-        'voices'), genders[gender], languages[language], accents[accent])
-
-    if len(filtered_voice_list):
-        # Displaying the details of filtered voices
-        print("AVAILABLE READERS")
-        for index, voice in enumerate(filtered_voice_list):
-            print("\nVoice Index : %d" % index)
-            print("ID: %s" % voice.id)
-            print("Name: %s" % voice.name)
-            print("Age: %s" % voice.age)
-            print("Gender: %s" % voice.gender)
-            print("Languages Known: %s" % voice.languages)
-
-        index = 0
-        try:
-            if args.index and filtered_voice_list[int(args.index)]:
-                index = int(args.index)
-        except IndexError:
-            pass
-
-        print("\n%s is reading for you." % filtered_voice_list[index].name)
-        # applying the voice of selected reader
-        reader.setProperty('voice', filtered_voice_list[index].id)
-    else:
-        print("No reader available.\nAlex is reading for you.")
-
+# ...
 
 try:
-    # initialize the reading engine
-    reader = pyttsx3.init()
-    
-    # updating readers language, accent and gender before reading text.
-    update_language(reader, language, accent, gender)
+    # ...
 
-    # read the given text
-    reader.say(text_to_read)
+    # updating readers accent and gender before reading text.
+    update_language(reader, "english", accent, gender)
 
-    # Execution of reading process
+    # save spoken text to an audio file
+    reader.save_to_file(text_to_read, output_file)
+
+    # Execution of saving process
     reader.runAndWait()
 
-    # Finish the reading process
+    print(f"Audio saved to {output_file}")
+
+    # Finish the saving process
     reader.stop()
 
 except OSError as error:
@@ -114,4 +63,7 @@ except OSError as error:
 
 except Exception as error:
     traceback.print_exception(*sys.exc_info())
-    print("Someting went wrong; please report the issue at https://github.com/vishalnagda1/text-to-speech/issues")
+    print("Something went wrong; please report the issue at https://github.com/vishalnagda1/text-to-speech/issues")
+
+    
+python script_name.py -t "Hello, this is a test." -o output_audio.mp3
